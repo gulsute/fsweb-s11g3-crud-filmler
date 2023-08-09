a86b40b5c258c57b95a2ff20cfa7d4e39c610cd9
gulsute <134259376+gulsute@users.noreply.github.com>
2023-08-09 15:40:27 +0300

    film ekleme tamamlandı

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import FavoriteMovieList from "./components/FavoriteMovieList";
 
 import axios from "axios";
 import EditMovieForm from "./components/EditMovieForm";
+import AddMovieForm from "./components/AddMovieForm";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
@@ -49,6 +50,10 @@ const App = (props) => {
           <Switch>
             <Route path="/movies/edit/:id">
               <EditMovieForm setMovies={setMovies} />
+            </Route>
+
+            <Route path="/movies/add">
+              <AddMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
